521b1a388714370783f835c0c9fd278025ae0cab
Felipe Anjos <fanjos.santos@outlook.com>
2025-08-09 15:12:41 -0300

    primeiro teste

---
+++ test/controller/transferController.test.js
@@ -0,0 +1,21 @@
+// Libraries
+const request = require('supertest');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+// Application
+const app = require('../../app');
+
+// Testes
+describe('Transfer Controller', () => {
+  describe('POST /transfers', () => {
+    it('Quando informo remetente e destinatário inexistentes, o retorno será 400', async () => {
+      const resposta = await request(app).post('/transfers').send({
+        from: "julio",
+        to: "priscila",
+        value: 100
+      });
+      expect(resposta.status).to.equal(400);
+    });
+  });
+});
