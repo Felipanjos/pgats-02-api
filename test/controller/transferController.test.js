78babfb05cd70f621d4a329a0470717eb6036fd4
Felipe Anjos <fanjos.santos@outlook.com>
2025-08-22 21:16:41 -0300

    Adicionando um novo teste de erro na transferência com Mock

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -22,7 +22,7 @@ describe('Transfer Controller', () => {
       expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado');
     });
 
-    it('Quando informo remetente e destinatário inexistentes, o retorno será 400', async () => {
+    it('Usando Mocks: Quando informo remetente e destinatário inexistentes, o retorno será 400', async () => {
       // Mockar apenas a função transfer do service
       const transferServiceMock = sinon.stub(transferService, 'transfer');
       transferServiceMock.throws(new Error('Usuário remetente ou destinatário não encontrado'));
@@ -34,6 +34,25 @@ describe('Transfer Controller', () => {
       });
       expect(resposta.status).to.equal(400);
       expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado');
+
+      // Resetar o Mock
+      sinon.restore();
+    });
+
+    it('Usando Mocks: Quando informo valores válidos, o retorno será 201', async () => {
+      // Mockar apenas a função transfer do service
+      const transferServiceMock = sinon.stub(transferService, 'transfer');
+      transferServiceMock.returns({ from: 'julio', to: 'priscila', value: 100, date: new Date().toISOString });
+
+      const resposta = await request(app).post('/transfers').send({
+        from: 'julio',
+        to: 'priscila',
+        value: 100,
+      });
+      expect(resposta.status).to.equal(201);
+      expect(resposta.body).to.have.property('from', 'julio');
+      expect(resposta.body).to.have.property('to', 'priscila');
+      expect(resposta.body).to.have.property('value', 100);
 
       // Resetar o Mock
       sinon.restore();
